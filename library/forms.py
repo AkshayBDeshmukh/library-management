517b49802c47f9dade29ffa77ac5126c533c346e
AkshayBDeshmukh <114093342+AkshayBDeshmukh@users.noreply.github.com>
2022-09-30 09:36:33 +0530

    Update forms.py

--- library/forms.py
+++ library/forms.py
@@ -32,7 +32,7 @@ class BookForm(forms.ModelForm):
         model=models.Book
         fields=['name','isbn','author','category']
 class IssuedBookForm(forms.Form):
-    #to_field_name value will be stored when form is submitted.....__str__ method of book model will be shown there in html
+  
     isbn2=forms.ModelChoiceField(queryset=models.Book.objects.all(),empty_label="Name and isbn", to_field_name="isbn",label='Name and Isbn')
     enrollment2=forms.ModelChoiceField(queryset=models.StudentExtra.objects.all(),empty_label="Name and enrollment",to_field_name='enrollment',label='Name and enrollment')
     
