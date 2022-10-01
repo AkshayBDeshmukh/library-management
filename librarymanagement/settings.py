097b036ae06fe082d85b5fdc36f76f547914b9d6
AkshayBDeshmukh <114093342+AkshayBDeshmukh@users.noreply.github.com>
2022-10-01 12:25:21 +0530

    Update settings.py

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -91,9 +91,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 STATIC_DIR,
